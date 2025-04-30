86ea959fc7c7508eea4ec35b0c21a8fee86ea664
Alejandro <alejandro.flores.a@ucb.edu.bo>
2025-04-29 23:29:06 -0400

    script js modificado

--- script.js
+++ script.js
@@ -2,5 +2,13 @@
 const verDetallesButton = document.getElementById('verDetalles');
 const volverButton = document.getElementById('volver');
 const tarjeta = document.querySelector('.tarjeta');
-// Función para voltear la tarjeta
+
+// Función para mostrar cara posterior
+verDetallesButton.addEventListener('click', () => {
+  tarjeta.classList.add('girada');
+});
+
 // Función para volver a la cara frontal
+volverButton.addEventListener('click', () => {
+  tarjeta.classList.remove('girada');
+});
